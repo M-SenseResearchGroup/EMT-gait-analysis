73945a6d6d85ea71b2d5c2dc46c0a969c5a4e8a2
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-07-30 11:25:27 -0400

    Added repeated measures ANOVA to stair_climb_stats

--- Stair_climb_stats.py
+++ Stair_climb_stats.py
@@ -141,20 +141,94 @@ for subj in subjs:
                 bdd_stats[subj][c[0]][i] = 999  # data is missing from something
         # print(f'{subj}  {c[0]}\n {bdd_stats[subj][c[0]]}\n')
 
-fid = open('..\\Data\\stair_climb_stats_prelim.csv', 'w')
+# fid = open('..\\Data\\stair_climb_stats_prelim.csv', 'w')
+#
+# fid.write('Subject,Direction,Comparison,Step_Length,Lateral_Dev,Step_Height,' +
+#           'Max_Swing_Vel,Foot_Attack_Angle,Contact_Time,Step_Time,Cadence\n')
+#
+# for subj in subjs:
+#     for i, v in zip([1, 2], [bda_stats, bdd_stats]):
+#         for c in compars:
+#             line = f'{int(subj)},{i},{c[0]}'
+#             for j in range(8):
+#                 line += f',{v[subj][c[0]][j]}'
+#             line += '\n'
+#             fid.write(line)
+#
+# fid.close()
 
-fid.write('Subject,Direction,Comparison,Step_Length,Lateral_Dev,Step_Height,' +
-          'Max_Swing_Vel,Foot_Attack_Angle,Contact_Time,Step_Time,Cadence\n')
 
-for subj in subjs:
-    for i, v in zip([1, 2], [bda_stats, bdd_stats]):
-        for c in compars:
-            line = f'{int(subj)},{i},{c[0]}'
-            for j in range(8):
-                line += f',{v[subj][c[0]][j]}'
-            line += '\n'
-            fid.write(line)
+# ANOVA
 
-fid.close()
+# Repeated measures ANOVA
+
+def avg_stuff(data, subs, wd, m=0):
+    savg = np.zeros((len(subs), 4))  # 4 different bag types
+
+    rem = []  # indices to be removed if subj contains no data
+    for sub, i in zip(subs, range(len(subs))):
+        for bt, j in zip(['hb', 'lb', 'hs', 'ls'], range(4)):
+            try:
+                savg[i, j] = np.mean(data[sub][bt][wd][:, m])
+            except IndexError:
+                rem.append((i, j))
+
+    for inds in rem:
+        i, j = inds
+        savg = np.delete(savg, i, axis=j)
+    return savg
 
 
+def rmANOVA(data):
+    """
+    Repeated Measures ANOVA
+
+    data : array_like
+        MxN array where M is the number of subjects, and N is the number of repeated measures
+    """
+
+    # TODO add sphericity check/corrections
+    # TODO add normality check
+    # TODO add option to not use all columns
+
+    N = data.size  # number of elements
+    s, a = data.shape  # number of subjects, number of repeated measures (time points)
+
+    dfb = a-1  # Degrees of freedom (DoF) between
+    dfw = N-a  # DoF within
+    dfs = s-1  # DoF subjects
+    dfe = dfw - dfs  # DoF error
+    dft = N-1  # DoF total
+
+    ssb = np.sum(np.sum(data, axis=0)**2)/s - np.sum(data)**2/N  # SS_between
+    ssw = np.sum(data**2) - np.sum(np.sum(data, axis=0)**2)/s  # SS_within
+    sss = np.sum(np.sum(data, axis=1)**2)/a - np.sum(data)**2/N  # SS_subjects
+    sse = ssw - sss  # SS_error
+    sst = np.sum(data**2) - np.sum(data)**2/N  # SS_total
+
+    msb = ssb/dfb  # MS_between
+    msw = ssw/dfw  # MS_within
+    mss = sss/dfs  # MS_subject
+    mse = sse/dfe  # MS_error
+    mst = sst/dft  # MS_total
+
+    F = msb/mse  # F-statistic
+
+    p = stats.f.sf(F, dfb, dfe)
+
+    eta_sq = ssb/(ssb + sse)
+
+    # return (dfb, msb), (dfw, msw), (dfs, mss), (dfe, mse), (dft, mst)
+    return F, p, eta_sq
+
+
+metrics = ['step length', 'lateral deviation', 'step height', 'max swing velocity', 'foot attack angle', 'contact time',
+           'step time', 'cadence']
+for mm, met in zip(range(8), metrics):
+    uavg = avg_stuff(comb, subjs, 'up', m=mm)
+    davg = avg_stuff(comb, subjs, 'down', m=mm)
+
+    Fu, pu, eu = rmANOVA(uavg)
+    Fd, pd, ed = rmANOVA(davg)
+
+    print(f'{met:19s}   Up: p={pu:.3f} eta^2={eu:.4f}   Down: p={pd:.3f} eta^2={ed:.4f}')
