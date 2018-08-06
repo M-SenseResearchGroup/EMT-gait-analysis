df66316fbe9155dd41f7e16b8a2264bb58fce430
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-08-06 12:46:12 -0400

    Changed to opening serialized data

--- WT_analysis.py
+++ WT_analysis.py
@@ -11,7 +11,9 @@ import MC10py
 # import matplotlib.pyplot as pl
 
 # def load_mc10(study_dir, segment=True, sync=True, save=True, save_loc=None, save_subj=False, return_data=True):
-paths = MC10py.LoadMC10('C:\\Users\\Lukas Adamowicz\\Documents\\Study Data\\EMT\\ASYM_OFFICIAL', segment=True,
-                        sync=True, save=True, save_loc='import', save_subj=False, return_data=False)
+# paths = MC10py.LoadMC10('C:\\Users\\Lukas Adamowicz\\Documents\\Study Data\\EMT\\ASYM_OFFICIAL', segment=True,
+#                         sync=True, save=True, save_loc='import', save_subj=False, return_data=False)
+
+data = MC10py.OpenMC10('C:\\Users\\Lukas Adamowicz\\Documents\\Study Data\\EMT\\ASYM_OFFICIAL\\data.pickle')
 
 # bl, al = butter(1, 8/62.5, 'lowpass')  # half order since will use filtfilt
